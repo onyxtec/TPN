2d312929b9007a010b41f70646d20c06f711410b
faa-onyxtec <faa@onyxtec.co>
2022-07-05 16:47:54 +0500

    Registration in vue

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\exapmle;
 use Illuminate\Support\Facades\Route;
@@ -20,10 +21,11 @@ use Illuminate\Support\Facades\Auth;
 // Auth routes
 Route::get('/login', [LoginController::class,'showLoginForm'])->name('login');
 Route::post('post/login',[LoginController::class,'login'])->name('login.post');
+Route::get('register', [RegisterController::class,'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
 
 Route::group(['middleware' => 'auth'], function () {
     //Dashboard Routes
     Route::post('/logout',[LoginController::class,'logout'])->name('logout');
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 });
-Route::get('abc', [exapmle::class , 'index']);
