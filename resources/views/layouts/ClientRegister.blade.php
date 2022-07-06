7c3695bf58eae75474adde430548cfb48f617211
faa-onyxtec <faa@onyxtec.co>
2022-07-06 12:06:58 +0500

    Client Login and registeration

---
+++ resources/views/layouts/ClientRegister.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<client-register-component>
+
+</client-register-component>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ClientLoginController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\exapmle;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,13 +19,21 @@ use Illuminate\Support\Facades\Auth;
 */
 
 // Auth routes
-Route::get('/login', [LoginController::class,'showLoginForm'])->name('login');
-Route::post('post/login',[LoginController::class,'login'])->name('login.post');
+// Route::get('/login', [LoginController::class,'showLoginForm'])->name('login');
+// Route::post('post/login',[LoginController::class,'login'])->name('login.post');
+Route::get('/', [LoginController::class,'showLoginForm'])->name('login');
+Route::post('/login',[LoginController::class,'login'])->name('login/post');
 Route::get('register', [RegisterController::class,'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
+Route::get('home', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['middleware' => 'auth'], function () {
-    //Dashboard Routes
+// Route::group(['middleware' => 'auth'], function () {
+    // Dashboard Routes
     Route::post('/logout',[LoginController::class,'logout'])->name('logout');
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-});
+    // Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+// });
+Route::get('client/login', [ClientLoginController::class,'index'])->name('client/login');
+Route::post('client/post-login',[ClientLoginController::class,'login'])->name('clientLogin.post');
+Route::get('client/register', [ClientLoginController::class,'register'])->name('client/register');
+Route::post('client/register', [ClientLoginController::class, 'registration']);
+
