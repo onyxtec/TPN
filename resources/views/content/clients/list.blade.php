b541f1081ffe3f87caa2f1cd6639d2d87bf4b2c6
samra-abbas <sa@onyxtecco>
2022-07-13 17:13:42 +0500

    client crud

---
+++ resources/views/content/clients/list.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'Clients List')
+
+@section('vendor-style')
+    {{-- Page Css files --}}
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap4.min.css')) }}">
+@endsection
+
+@section('page-style')
+    {{-- Page Css files --}}
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-user.css')) }}">
+@endsection
+
+@section('content')
+    <!-- users list start -->
+    <section class="app-user-list">
+        <!-- list section start -->
+        <div class="card">
+            <div class="card-datatable table-responsive pt-0">
+                <table class="clients-list-table">
+                    <thead class="thead-light">
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Contact Number</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+        <!-- list section end -->
+    </section>
+    <!-- users list ends -->
+@endsection
+
+@section('vendor-script')
+    {{-- Vendor js files --}}
+    <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap4.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.bootstrap4.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
+@endsection
+
+@section('page-script')
+    {{-- Page js files --}}
+    <script src="{{ asset(mix('js/scripts/pages/app-clients-list.js')) }}"></script>
+    <script>
+        $("body").on("click", "#deleteClient", function(e) {
+            var id = $(this).data("id");
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-outline-danger ml-1'
+                },
+                buttonsStyling: false
+            }).then(function(result) {
+                if (result.value) {
+                    $.ajaxSetup({
+                        headers: {
+                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                        },
+                    });
+                    $.ajax({
+                        url: 'coffee/' + id,
+                        type: "DELETE",
+                        success: function(response) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Deleted!',
+                                text: 'Coffee has been deleted.',
+                                customClass: {
+                                    confirmButton: 'btn btn-success'
+                                }
+                            }).then(function() {
+                                location.reload();
+                            });
+                        },
+                        error: function(xhr) {
+                            alert('Error while deleting product');
+                        }
+                    });
+                    //   } else if (result.dismiss === Swal.DismissReason.cancel) {
+                    //     Swal.fire({
+                    //       title: 'Cancelled',
+                    //       text: 'You prevented the deletion.',
+                    //       icon: 'error',
+                    //       customClass: {
+                    //         confirmButton: 'btn btn-success'
+                    //       }
+                    //     });
+                }
+            });
+        });
+    </script>
+@endsection
