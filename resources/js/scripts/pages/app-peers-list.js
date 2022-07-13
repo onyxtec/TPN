ab2beb328377b557cea0b37d997ad4c69c790780
samra-abbas <sa@onyxtecco>
2022-07-13 15:44:28 +0500

    peer crud

---
+++ resources/js/scripts/pages/app-peers-list.js
@@ -0,0 +1,104 @@
+$(function () {
+    'use strict';
+    var dtPeersTable = $('.peers-list-table');
+    // Customers List datatable
+    if (dtPeersTable.length) {
+      dtPeersTable.DataTable({
+        ajax: "/peers", // JSON file to add data
+        columns: [
+          // columns according to JSON
+          { data: "id" },
+          { data: "fullName" },
+          { data: "email" },
+          { data: "contact_no" },
+        ],
+        columnDefs: [
+          {
+            targets: 0,
+            visible:false
+          },
+         {
+             // Actions
+             targets: 4,
+             title: 'Actions',
+             orderable: false,
+             render: function (data, type, full, meta) {
+               return (
+                     '<div class="btn-group">' +
+                     '<a class="btn btn-sm dropdown-toggle hide-arrow" data-toggle="dropdown">' +
+                 feather.icons["more-vertical"].toSvg({ class: "font-small-4" }) +
+                     "</a>" +
+                     '<div class="dropdown-menu dropdown-menu-right">' +
+                     '<a href="' +
+                     "peers/" +
+                     full["id"] +
+                     '" class="dropdown-item">' +
+                     feather.icons["file-text"].toSvg({
+                         class: "font-small-4 mr-50",
+                     }) +
+                     "Details</a>" +
+                     '<a href="' +
+                     "peers/" +
+                     full["id"] +
+                    "/edit" +
+                     '" class="dropdown-item">' +
+                     feather.icons["archive"].toSvg({ class: "font-small-4 mr-50" }) +
+                     "Edit</a>" +
+                     '<a id="deletePeer" data-id="' +
+                             full["id"] +
+                     '" class="dropdown-item delete-record">' +
+                     feather.icons["trash-2"].toSvg({ class: "font-small-4 mr-50" }) +
+                     "Delete</a></div>" +
+                     "</div>" +
+                     "</div>"
+                 );
+             }
+           }
+        ],
+        order: [[2, 'desc']],
+        dom:
+          '<"d-flex justify-content-between align-items-center header-actions mx-1 row mt-75"' +
+          '<"col-lg-12 col-xl-6" l>' +
+          '<"col-lg-12 col-xl-6 pl-xl-75 pl-0"<"dt-action-buttons text-xl-right text-lg-left text-md-right text-left d-flex align-items-center justify-content-lg-end align-items-center flex-sm-nowrap flex-wrap mr-1"<"mr-1"f>B>>' +
+          '>t' +
+          '<"d-flex justify-content-between mx-2 row mb-1"' +
+          '<"col-sm-12 col-md-6"i>' +
+          '<"col-sm-12 col-md-6"p>' +
+          '>',
+        language: {
+          sLengthMenu: 'Show _MENU_',
+          search: 'Search',
+          searchPlaceholder: 'Search..'
+        },
+        // Buttons with Dropdown
+        buttons: [
+          {
+            attr: {
+              'data-toggle': 'modal',
+              'data-target': '#modals-slide-in'
+            },
+            init: function (api, node, config) {
+              $(node).removeClass('btn-secondary');
+            }
+          }
+        ],
+        // For responsive popup
+        language: {
+          paginate: {
+            // remove previous & next text from pagination
+            previous: '&nbsp;',
+            next: '&nbsp;'
+          }
+        },
+      });
+    }
+    $('body').tooltip({
+        selector: '[data-toggle="tooltip"]',
+        container: 'body'
+      });
+  });
+  
+  
+  
+  
+  
---
+++ resources/views/content/peers/view.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'Peer View')
+
+@section('vendor-style')
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap4.min.css')) }}">
+@endsection
+@section('page-style')
+    {{-- Page Css files --}}
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-invoice-list.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-user.css')) }}">
+@endsection
+
+@section('content')
+    <section class="app-user-view">
+        <!-- Coffee Card & Plan Starts -->
+        <div class="row">
+            <!-- Coffee Card starts-->
+            <div class="col-xl-9 col-lg-8 col-md-7">
+                <div class="card user-card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-12 d-flex flex-column justify-content-between border-container-lg">
+                                <div class="user-avatar-section">
+                                    <div class="d-flex justify-content-start">
+                                        <img class="img-fluid rounded" src="{{ asset('images/avatars/user.png') }}"
+                                            height="104" width="104" alt="No image" />
+                                        <div class="d-flex flex-column ml-1">
+                                            <div class="peers mb-1">
+                                                <h4 class="mb-0">{{ $peers['fullName'] }}</h4>
+                                                <span class="card-text">{{ $peers['email'] }}</span>
+                                            </div>
+                                            <div class="d-flex flex-wrap">
+                                                <a class="btn btn-primary mr-2 my-2"
+                                                    href="{{ route('peers.index') }}">Back</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-12 mt-2 mt-xl-0">
+                                <div class="user-info-wrapper">
+                                    <div class="d-flex flex-wrap">
+                                        <div class="user-info-title">
+                                            <i data-feather="user" class="mr-1"></i>
+                                            <span class="card-text user-info-title font-weight-bold mb-0">Peer Name</span>
+                                        </div>
+                                        <p class="card-text mb-0">{{ $peers['fullName'] }}</p>
+                                    </div>
+                                    <div class="d-flex flex-wrap my-50">
+                                        <div class="user-info-title">
+                                            <i data-feather="phone" class="mr-1"></i>
+                                            <span class="card-text user-info-title font-weight-bold mb-0">Contact
+                                                Number</span>
+                                        </div>
+                                        <p class="card-text mb-0">{{ $peers['contact_no'] }}</p>
+                                    </div>
+                                    <div class="d-flex flex-wrap my-50">
+                                        <div class="user-info-title">
+                                            <i data-feather="calendar" class="mr-1"></i>
+                                            <span class="card-text user-info-title font-weight-bold mb-0">DOB</span>
+                                        </div>
+                                        <p class="card-text mb-0">{{ $peers['dob'] }}</p>
+                                    </div>
+                                    <div class="d-flex flex-wrap my-50">
+                                        <div class="user-info-title">
+                                            <i data-feather="home" class="mr-1"></i>
+                                            <span class="card-text user-info-title font-weight-bold mb-0">Address</span>
+                                        </div>
+                                        <p class="card-text mb-0">{{ $peers['address'] }}</p>
+                                    </div>
+                                    <div class="d-flex flex-wrap my-50">
+                                        <div class="user-info-title">
+                                            <i data-feather="check-square" class="mr-1"></i>
+                                            <span
+                                                class="card-text user-info-title font-weight-bold mb-0">Specialization</span>
+                                        </div>
+                                        <p class="card-text mb-0">
+                                            @if ($peers['specialization_type'] == 'Both')
+                                                <ul style="padding-left: 6px">
+                                                    <li> MentalHealth</li>
+                                                    <li>{{ $peers['sub_type'] }}</li>
+                                                </ul>
+                                            @elseif($peers['specialization_type'] == 'Substance Use Disorder')
+                                                {{ $peers['sub_type'] }}
+                                            @else
+                                                {{ $peers['specialization_type'] }}
+                                            @endif
+                                        </p>
+                                    </div>
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Coffee Card Ends-->
+
+
+        </div>
+        <!-- Coffee Card & Plan Ends -->
+
+
+
+
+    </section>
+@endsection
+
+@section('vendor-script')
+    <script src="{{ asset(mix('vendors/js/extensions/moment.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/jquery.dataTables.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap4.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.bootstrap4.min.js')) }}"></script>
+@endsection
+@section('page-script')
+    {{-- Page js files --}}
+    {{-- <script src="{{ asset(mix('js/scripts/pages/app-user-view.js')) }}"></script> --}}
+@endsection
