6bb2b868b193c0be7dc6c98334a97dfb0498b74f
samra-abbas <sa@onyxtecco>
2022-07-27 15:48:50 +0500

    Setting of files

--- resources/views/customer/client/booking.blade.php
+++ resources/views/customer/client/booking.blade.php
@@ -1,25 +1,10 @@
 @extends('customer/home')
-@section('vendor-style')
-  {{-- Page Css files --}}
-  <link rel="stylesheet" href="{{ asset('vendors/css/tables/datatable/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/tables/datatable/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('vendors/css/tables/datatable/buttons.bootstrap4.min.css') }}">
-@endsection
-
-@section('page-style')
-  {{-- Page Css files --}}
-  <link rel="stylesheet" href="{{ asset('css/base/plugins/forms/form-validation.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/base/pages/app-user.css') }}">
-  
-@endsection
 @section('main-section')
 
 <booking-component>
 </booking-component>
 
 @endsection
-@section('page-script')
-<script src="{{asset(mix('customer-resources/js/scripts/pages/app-user-list.js'))}}"></script>
-@endsection
+
 
 
